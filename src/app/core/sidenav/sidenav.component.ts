3d79a46c2886794c705d873d51c0732f9bc09846
amanyShaker9 <58103474+amanyShaker9@users.noreply.github.com>
2022-03-21 01:39:33 +0200

    firstcommit

---
+++ src/app/core/sidenav/sidenav.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {FlatTreeControl} from '@angular/cdk/tree';
+
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Appointment',
+    children: [{name: 'View appointment'},{name: 'Book appointment'},],
+  },
+  {
+    name: 'Doctors',
+    children: [{name: 'All Doctor'},{name: 'Add Doctor'},],
+  },
+  {
+    name: 'Patients',
+    children: [{name: 'All Patient'},{name: 'Add Patient'},],
+  },
+  {
+    name: 'Staff',
+    children: [{name: 'All Staff'},{name: 'Add Employee'},],
+  },
+  {
+    name: 'Billing',
+    children: [{name: 'Bill List'},{name: 'Add Bill'},{name: 'Invoice'},],
+  },
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  constructor() { 
+    this.dataSource.data = TREE_DATA;
+  }
+
+  ngOnInit(): void {
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+}
