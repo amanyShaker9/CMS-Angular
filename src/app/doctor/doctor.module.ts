e9e17aca1ec9d64f1c9526e171bfa53390da5518
01274897993esraasamir@gmail.com <01274897993esraasamir@gmail.com>
2022-03-22 08:20:41 +0200

    add doctor-component

--- src/app/doctor/doctor.module.ts
+++ src/app/doctor/doctor.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
+import { DoctorListComponent } from './doctor-list/doctor-list.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { DeleteDoctorComponent } from './delete-doctor/delete-doctor.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddDoctorComponent,
+    EditDoctorComponent,
+    DoctorListComponent,
+    DoctorProfileComponent,
+    DeleteDoctorComponent
+  ],
   imports: [
     CommonModule
   ]
